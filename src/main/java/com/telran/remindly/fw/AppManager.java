17facb1e76f8eb97fcf757eda9b276333cc290fd
Ira Baker <49585685+irinabaker@users.noreply.github.com>
2022-09-13 09:14:45 +0200

    Added MainScreenTests

---
+++ src/main/java/com/telran/remindly/fw/AppManager.java
@@ -0,0 +1,38 @@
+package com.telran.remindly.fw;
+
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.android.AndroidDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.concurrent.TimeUnit;
+
+public class AppManager {
+
+    AppiumDriver driver;
+
+    DesiredCapabilities capabilities;
+
+    MainScreenHelper mainScreen;
+
+    public MainScreenHelper getMainScreen() {
+        return mainScreen;
+    }
+
+    public void init() throws MalformedURLException {
+        capabilities = new DesiredCapabilities();
+        capabilities.setCapability("platformName", "Android");
+        capabilities.setCapability( "platformVersion", "8.0.0");
+        capabilities.setCapability( "deviceName", "qa_mob");
+        capabilities.setCapability( "automationName", "Appium");
+        capabilities.setCapability( "appPackage", "com.blanyal.remindly");
+        capabilities.setCapability(  "appActivity", "com.blanyal.remindme.MainActivity");
+        capabilities.setCapability(  "app", "C:/Users/Tel-Ran.de/Documents/apk/com.blanyal.remindly_2_apps.evozi.com.apk");
+
+        driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"),capabilities);
+        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+
+        mainScreen = new MainScreenHelper(driver);
+    }
+}
---
+++ src/test/java/com/telran/remindly/tests/MainScreenTests.java
@@ -0,0 +1,11 @@
+package com.telran.remindly.tests;
+
+import org.testng.annotations.Test;
+
+public class MainScreenTests extends TestBase{
+
+    @Test
+    public void appLaunchTest() {
+
+    }
+}
